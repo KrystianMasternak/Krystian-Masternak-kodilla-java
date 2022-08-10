fd7d99f9670af4602fd9d4ff285f77cb08769ad0
Krystian Masternak <k.masternak95@gmail.com>
2022-08-10 15:37:44 +0200

    6.4. Writing tests with JUnit

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -7,18 +7,24 @@ public class ShapeCollector {
     private List<Shape> shapeList = new ArrayList<>();
 
     public void addFigure(Shape shape){
+       shapeList.add(shape);
 
     }
     public boolean removeFigure(Shape shape){
-        // return temporarily true
+        boolean result = false;
+        if(shapeList.contains(shape)){
+            shapeList.remove(shape);
+        }
         return true;
     }
     public Shape getFigure(int n){
-
+        if(n >= 0 && n < shapeList.size()){
+            return shapeList.get(n);
+        }
         return null;
     }
     public void showFigures(){
-
+        System.out.println(shapeList.toString());
     }
     public List<Shape> getShapeList(){
         return shapeList;
