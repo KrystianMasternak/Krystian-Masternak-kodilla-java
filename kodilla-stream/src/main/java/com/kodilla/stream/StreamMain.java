5415a2fa5bcb55044188e994520c6bf7eb0ab609
Krystian Masternak <k.masternak95@gmail.com>
2022-08-15 15:59:08 +0200

    7.2. What is Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -24,20 +25,22 @@ public class StreamMain {
         //expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        //PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        String text1 = poemBeautifier.beautify("HELLO!", (a -> a.toLowerCase()));
-        System.out.println(text1);
+        //String text1 = poemBeautifier.beautify("HELLO!", (a -> a.toLowerCase()));
+        //System.out.println(text1);
 
-        String text2 = poemBeautifier.beautify("The earth ", (a -> a.concat("is ").concat("round!!")));
-        System.out.println(text2);
+        //String text2 = poemBeautifier.beautify("The earth ", (a -> a.concat("is ").concat("round!!")));
+        //System.out.println(text2);
 
-        String text3 = poemBeautifier.beautify("World is beautiful", (a -> a.toUpperCase()));
-        System.out.println(text3);
+        //String text3 = poemBeautifier.beautify("World is beautiful", (a -> a.toUpperCase()));
+        //System.out.println(text3);
 
-        String text4 = poemBeautifier.beautify("PoemBeautifier", (a -> a.substring(4,14)));
-        System.out.println(text4);
+        //String text4 = poemBeautifier.beautify("PoemBeautifier", (a -> a.substring(4,14)));
+        //System.out.println(text4);
 
+        //System.out.println("Using Stream to generate even numbers from 1 to 20");
+        //NumbersGenerator.generateEven(20);
 
 
 
