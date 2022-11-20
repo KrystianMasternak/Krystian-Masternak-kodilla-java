c6ef00386357b404636b08e32c86ff2a755cae31
Krystian Masternak <k.masternak95@gmail.com>
2022-11-20 12:26:28 +0100

    16.3. SQL - creating and modifying objects

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -15,6 +15,7 @@ public class SimpleInvoiceTestSuite {
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99), 3.5));
         invoice.addItem(new SimpleItem(new SimpleProduct("Product 3",  6.49), 5.0));
 
+
         //Then
         Assertions.assertEquals(108.975, invoice.getValueToPay(), 0.001);
     }
