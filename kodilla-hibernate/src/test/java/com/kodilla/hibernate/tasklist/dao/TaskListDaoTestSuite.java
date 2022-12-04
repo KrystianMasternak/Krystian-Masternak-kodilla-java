e3e66d4eb95efa5abb51fc7a506771fa989f2274
Krystian Masternak <k.masternak95@gmail.com>
2022-12-04 15:12:20 +0100

    17.2. We create entities

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //Given
+
+    }
+}
